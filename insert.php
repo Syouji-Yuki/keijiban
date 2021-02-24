393e7c3c3330c211d5784f29595a75754c16617b
Syouji-Yuki <1026yukisyouji@gmail.com>
2021-02-24 17:36:12 +0900

    掲示板

---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+
+mb_internal_encoding("utf8");
+
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+
+$pdo->exec("insert into 4each_keijiban(handlename,title,comments)
+values('".$_POST['handlename']."','".$_POST['title']."','".$_POST['comments']."');");
+
+header("Location:http://localhost/4each_keijiban/index.php");
+
+?>
+
+<!DOCTYPE html>
+<heml lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+</heml>
